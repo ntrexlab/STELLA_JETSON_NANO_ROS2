0dc7265a6b0a36d9d850c7f137ca82299f8f13a5
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2021-10-12 15:44:47 +0900

    Update create_udev_rules.sh

--- stella_bringup/create_udev_rules.sh
+++ stella_bringup/create_udev_rules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, AHRS, Motordriver, Bluetooth"
-echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT  , check it using the command : ls -l /dev|grep -e ttyUSB -e ttyS0"
+echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT  , check it using the command : ls -l /dev|grep -e ttyUSB -e ttyTHS1"
 echo "start copy stella.rules to  /etc/udev/rules.d/"
 echo "$HOME/colcon_ws/src/STELLA_JETSON_NANO_ROS2/stella_bringup/stella.rules"
 sudo cp $HOME/colcon_ws/src/STELLA_JETSON_NANO_ROS2/stella_bringup/stella.rules  /etc/udev/rules.d
