0e46f060496b65c9e3f4852648c80e97e4275847
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2021-10-12 16:04:11 +0900

    Add files via upload

--- stella_camera/stella_camera/stella_camera.py
+++ stella_camera/stella_camera/stella_camera.py
@@ -9,6 +9,33 @@ import os
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+def gstreamer_pipeline(
+    capture_width=320,
+    capture_height=240,
+    display_width=320,
+    display_height=240,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
  
 class Camera(Node):
 
@@ -23,7 +50,7 @@ class Camera(Node):
     self.timer = self.create_timer(timer_period, self.timer_callback)
     
     os.system('sudo modprobe bcm2835-v4l2')
-    self.cap = cv2.VideoCapture(0)
+    self.cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
     self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
     self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
     
